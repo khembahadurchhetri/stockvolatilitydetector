5ea6d2f847417a7b7fd82cc3a80c2e7850fc3e11
khembahadurchhetri <khembchhetri@student.pu.edu.np>
2026-09-09 23:01:06 +0545

    chore: remove .next folder from tracking and update gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gfirKW925KixQqnZqm3FLHpqOJiJH9qWo4oWIgvYwTE=\"\n}"
--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_es-toolkit_dist_16minl7._.js","static/chunks/node_modules_recharts_es6_util_1hlwdqm._.js","static/chunks/node_modules_recharts_es6_state_1_-xsv7._.js","static/chunks/node_modules_recharts_es6_cartesian_1atubof._.js","static/chunks/node_modules_recharts_es6_component_0na1-t3._.js","static/chunks/node_modules_recharts_es6_0si1qps._.js","static/chunks/node_modules_1zy8cye._.js","static/chunks/app_page_tsx_1s_43kl._.js"],
-    source: "entry"
-});
